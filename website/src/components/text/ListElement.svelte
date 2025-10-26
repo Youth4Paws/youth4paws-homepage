56c999650d3fc10f42fe28a5fc34b99bf05ebe2f
Opisek <darowski.kacper@gmail.com>
2025-10-26 11:30:58 +0100

    selectable bullet point

--- website/src/components/text/ListElement.svelte
+++ website/src/components/text/ListElement.svelte
@@ -12,7 +12,7 @@
     position: relative;
   }
 
-  span {
+  span.bullet {
     position: absolute;
     left: -.7em;
     transform: translateX(-50%);
@@ -21,11 +21,19 @@
     align-items: center;
     height: dimensions.$fontSize * dimensions.$fontLineHeight;
   } 
+
+  span.copy {
+    position: absolute;
+    font-size: 0;
+  }
 </style>
 
 <li lang="de">
   <span class="bullet">
     <PawPrint size={15} strokeWidth={2.5}/>
+  </span>
+  <span class="copy">
+    •
   </span>
   {@render children()}
 </li>
