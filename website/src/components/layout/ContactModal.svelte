f4e29ad82f602119f8e0b7190aeede3abc76f720
kenanhacisalihoglu <kenan.hacisalihoglu@tum.de>
2026-02-04 15:35:04 +0100

    changes

--- website/src/components/layout/ContactModal.svelte
+++ website/src/components/layout/ContactModal.svelte
@@ -3,7 +3,7 @@
   
   const email = "kenan.hacisalihoglu.stud@gmail.com";
   const phone = "+49 176 46294142";
-  const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSftSlOB8J_m34I5zA2lBahgkaYx9aPNAubKjSSk50eJXEvStg/viewform?usp=dialog";
+  const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfkZnRMkfYqmAogcmZO-QIvRO4pYBJaL3cyih6Q_iDFknKy0g/viewform?usp=dialog";
   
   let copiedEmail = $state(false);
   let copiedPhone = $state(false);
