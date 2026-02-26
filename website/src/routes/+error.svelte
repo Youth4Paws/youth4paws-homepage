afe85e25a10c7c2ccd78bb28dc30951ccd6d87df
Opisek <darowski.kacper@gmail.com>
2026-02-26 21:40:42 +0100

    error page

---
+++ website/src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import Button from "../components/interactive/Button.svelte";
+  import Paragraph from "../components/layout/Paragraph.svelte";
+
+  import Section from "../components/layout/Section.svelte";
+</script>
+
+<Section title={`Fehler ${page.status}`}>
+  <Paragraph>
+    {#if page.status === 404}
+      Der gesuchte Inhalt konnte nicht gefunden werden.
+    {:else}
+      Es ist ein Fehler aufgetreten.
+    {/if}
+  </Paragraph>
+  <!-- TODO: buttons -->
+</Section>
