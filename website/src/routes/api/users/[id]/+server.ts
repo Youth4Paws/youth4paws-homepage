15c6093ce69e6597e7fd9578a3edb17e664232c4
Opisek <darowski.kacper@gmail.com>
2026-08-08 22:52:42 +0200

    delete user endpoint

---
+++ website/src/routes/api/users/[id]/+server.ts
@@ -0,0 +1,40 @@
+import { error, json } from "@sveltejs/kit";
+import type { RequestEvent, RequestHandler } from "../$types";
+import { checkUserPermissions } from "$lib/common/permissions";
+import { db } from "$lib/server/db";
+import { permissionsTable, usersTable } from "$lib/server/schema";
+import { eq } from "drizzle-orm";
+import { Permission } from "../../../../types/permissions";
+import { isValidUUID } from "$lib/common/validation";
+import { and } from "drizzle-orm";
+
+export const DELETE: RequestHandler = async ({ locals, params }: RequestEvent) => {
+  // @ts-ignore
+  if (!checkUserPermissions([ Permission.ManageUsers ], locals.permissions)) return error(401);
+
+  // Check if the supplied ID is a valid UUID
+  if (!isValidUUID(params.id)) return error(400, `Invalid UUID '${params.id}'.`);
+
+  // Fetch target user
+  const targetUser = await db.select({
+    id: usersTable.id,
+    isAdmin: permissionsTable.permission
+  }).from(usersTable).where(eq(
+    usersTable.id, params.id
+  )).leftJoin(permissionsTable, and(
+    eq(permissionsTable.userId, usersTable.id),
+    eq(permissionsTable.permission, Permission.Admin)
+  ));
+
+  // If the user does not exist, fail
+  if (targetUser.length == 0) return error(404, "Specified user does not exsit.");
+
+  // If the user is an administrator, only they can delete their own account.
+  // @ts-ignore
+  if (targetUser[0].isAdmin !== null && locals.user.id !== targetUser[0].id) return error(401);
+
+  // Delete the user account
+  await db.delete(usersTable).where(eq(usersTable.id, targetUser[0].id));
+
+  return json({ message: "ok" });
+}
