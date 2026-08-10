669a249f483d229fd6a1b94b1da12db97b96a419
Opisek <darowski.kacper@gmail.com>
2026-08-10 23:51:01 +0200

    markdown experiment: custom logo renderer

--- src/routes/(authorized)/dashboard/(categories)/editor/+page.svelte
+++ src/routes/(authorized)/dashboard/(categories)/editor/+page.svelte
@@ -2,10 +2,12 @@
   import CodeMirror from "svelte-codemirror-editor";
   import { markdown } from "@codemirror/lang-markdown";
   import SvelteMarkdown, { defaultRenderers } from '@humanspeak/svelte-markdown'
-    import MarkdownLink from "../../../../../components/markdown/MarkdownLink.svelte";
-    import MarkdownHeading from "../../../../../components/markdown/MarkdownHeading.svelte";
-    import List from "../../../../../components/text/List.svelte";
-    import ListElement from "../../../../../components/text/ListElement.svelte";
+  import MarkdownLink from "../../../../../components/markdown/MarkdownLink.svelte";
+  import MarkdownHeading from "../../../../../components/markdown/MarkdownHeading.svelte";
+  import List from "../../../../../components/text/List.svelte";
+  import ListElement from "../../../../../components/text/ListElement.svelte";
+  import type { MarkedExtension } from 'marked'
+  import StylizedTextLogo from "../../../../../components/decoration/StylizedTextLogo.svelte";
 
   let value = $state("hello");
 
@@ -15,8 +17,31 @@
     heading: MarkdownHeading,
     listItem: ListElement,
     list: List,
-}
+  }
+
+  const y4pextension: MarkedExtension = ({
+    extensions: [
+      {
+        name: "y4p",
+        level: "inline",
+        start(src) {
+          return src.match(/y(outh)?(4|for)p(aws)?/i)?.index
+        },
+        tokenizer(src) {
+          const match = /^y(outh)?(4|for)p(aws)?/i.exec(src)
+          if (!match) return
+
+          return {
+            type: "y4p",
+            raw: match[0],
+            text: match[0],
+          }
+        }
+      }
+    ]
+  })
 </script>
+
 
 <style lang="scss">
   @use "../../../../../styles/mixins/layout.scss";
@@ -43,6 +68,10 @@
     <CodeMirror bind:value lang={markdown()} />
   </div>
   <div class="preview">
-    <SvelteMarkdown source={value} renderers={customRenderers} />
+    <SvelteMarkdown source={value} renderers={customRenderers} extensions={[y4pextension]}>
+      {#snippet y4p()}
+        <StylizedTextLogo/>
+      {/snippet}
+    </SvelteMarkdown>
   </div>
 </div>
