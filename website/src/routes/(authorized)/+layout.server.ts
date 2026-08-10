116bf9f6e3d1fbcadd44cad5ff14b608850fec11
Opisek <darowski.kacper@gmail.com>
2026-08-10 13:05:30 +0200

    fix dashboard being prerendered for client-side navigation

--- website/src/routes/(authorized)/+layout.server.ts
+++ website/src/routes/(authorized)/+layout.server.ts
@@ -1,1 +1,6 @@
-export const prerender = false;
+import { fail } from "@sveltejs/kit"
+ 
+export const load = async (event) => {
+  const session = await event.locals.auth()
+  if (!session?.user?.id) return fail(401, "Unauthorized")
+}
