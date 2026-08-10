c6f83fce2d5aef0106c135805975f4ece441c2f6
Opisek <darowski.kacper@gmail.com>
2026-08-10 20:20:00 +0200

    loading indicators for login/logout buttons

--- src/routes/(authorized)/logout/+page.svelte
+++ src/routes/(authorized)/logout/+page.svelte
@@ -3,20 +3,24 @@
   import Section from "../../../components/layout/Section.svelte";
   import { signOut } from "@auth/sveltekit/client";
   import Button from "../../../components/interactive/Button.svelte";
+
+  let loading = $state(false);
   
-  function logout() {
-    signOut({
-      callbackUrl: "/"
-    });
+  async function logout() {
+    if (loading) return;
+    loading = true;
+    await signOut({ callbackUrl: "/" });
+    loading = false;
   }
 
   onMount(() => {
+    loading = false;
     logout();
   })
 </script>
 
 <Section title="Ausloggen">
-  <Button onclick={logout} accent={true}>
+  <Button onclick={logout} accent={true} loading>
     Von Youth4Paws ausloggen
   </Button>
 </Section>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,18 +3,24 @@
   import Section from "../../components/layout/Section.svelte";
   import { signIn } from "@auth/sveltekit/client";
   import Button from "../../components/interactive/Button.svelte";
+
+  let loading = $state(false);
   
-  function login() {
-    signIn("authentik");
+  async function login() {
+    if (loading) return;
+    loading = true
+    await signIn("authentik");
+    loading = false;
   }
 
   onMount(() => {
+    loading = false;
     login();
   })
 </script>
 
 <Section title="Einloggen">
-  <Button onclick={login} accent={true}>
+  <Button onclick={login} accent={true} loading>
     Mit Youth4Paws SSO einloggen
   </Button>
 </Section>
